76a9932e0afdeeae54b380dfda2f955369ea5451
ssmevens <ssaunders@its-ia.com>
2025-04-02 12:53:58 -0500

    made a mistake

--- move-install.sh
+++ move-install.sh
@@ -3,12 +3,12 @@
 #this script removes sets up the install of the autoamted linux installs
 # delete the existing install folders
  
-sudo rsync -a --delete ./bookworm/ /var/www/html/install/bookworm
-sudo rsync -a --delete ./mint/ /var/www/html/install/mint
+sudo rsync -a --delete ./bookworm/ /var/www/install/bookworm
+sudo rsync -a --delete ./mint/ /var/www/install/mint
 
 # set permissions
-sudo chmod -R 755 /var/www/html/install
-sudo chown -R www-data:www-data /var/www/html/install
+sudo chmod -R 755 /var/www/install
+sudo chown -R www-data:www-data /var/www/install
 
 # restart nginx
 sudo systemctl restart nginx
