1892ed3d5eadbb934707ee46aa5ca9596b10a307
ssmevens <ssaunders@its-ia.com>
2025-04-16 10:05:47 -0500

    updates

--- move-install.sh
+++ move-install.sh
@@ -3,6 +3,10 @@
 #this script removes sets up the install of the autoamted linux installs
 # delete the existing install folders
  
+mkdir /var/www/installs
+mkdir /var/www/installs/bookworm
+mkfir /var/www/installs/mint
+
 sudo rsync -a --delete ./bookworm/ /var/www/installs/bookworm
 sudo rsync -a --delete ./mint/ /var/www/installs/mint
 
