d2f6313a67d569d05f5cec2d65acf063d00153a5
ssmevens <ssaunders@its-ia.com>
2025-04-18 11:19:13 -0500

    Added documentation and comments to setup-mint.sh script to explain each section's purpose and functionality. Improved readability and maintainability of the automation script.

--- mint/setup-mint.sh
+++ mint/setup-mint.sh
@@ -32,7 +32,6 @@ apt install google-chrome-stable -y
 #drivers/ multimedia 3rd party
 apt install -y ubuntu-restricted-extras
 apt install -y ubuntu-drivers-common
-ubuntu-drivers autoinstall -y
 apt install -y mint-meta-codecs
 
 
